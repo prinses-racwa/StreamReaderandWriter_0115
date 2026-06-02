04c5d735992ac594ca10038e95a9e8d29e1d86f6
prinses-racwa <rajwaamelia@gmail.com>
2026-06-02 18:04:24 +0700

    menutup file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -30,3 +30,5 @@ int main()
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
