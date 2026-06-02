d5e29be9707fdabb783ccc684de8018f16552546
prinses-racwa <rajwaamelia@gmail.com>
2026-06-02 18:03:29 +0700

    menambahkan looping

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -18,3 +18,10 @@ int main()
     outfile.open(NamaFile + ".txt", ios::out);
 
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //looping untuk menulis isi file
+    while (true)
+    {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
